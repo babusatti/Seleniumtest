998fa695212b6db4c6839bb92f984685fc319ff2
babusatti <33149941+babusatti@users.noreply.github.com>
2021-07-09 20:45:57 +0530

    Update DamoClass.java

--- src/test/java/ui/DamoClass.java
+++ src/test/java/ui/DamoClass.java
@@ -14,21 +14,21 @@ public class DamoClass {
 		// TODO Auto-generated method stub
 		
 
-		System.setProperty("webdriver.chrome.driver", "A:\\Installed-Softwares\\eclip\\browserdrivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "src\\test\\java\\ui\\chromedriver.exe");
 		
 
 		ChromeDriver driver = new ChromeDriver();
 		
 		driver.get("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
-		//driver.findElement(By.xpath("//input[@id='identifierId']")).sendKeys("77298");
-		driver.findElement(By.xpath("//input[@name='login']")).sendKeys("gggg@ggg.com");
-		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("77298");
-		driver.findElement(By.xpath("//input[@type='submit']")).click();
+		driver.findElement(By.xpath("//input[@id='identifierId']")).sendKeys("77298");
+		//driver.findElement(By.xpath("//input[@name='login']")).sendKeys("gggg@ggg.com");
+		//driver.findElement(By.xpath("//input[@id='password']")).sendKeys("77298");
+		//driver.findElement(By.xpath("//input[@type='submit']")).click();
 		
 		TakesScreenshot scrShot =((TakesScreenshot)driver);
 
                 File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
-               File DestFile=new File("B:\\fff.png");
+               File DestFile=new File("C:\\fff.png");
                 try {
 					FileUtils.copyFile(SrcFile, DestFile);
 				} catch (IOException e) {
